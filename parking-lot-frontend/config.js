fa6123d03d7422625d9f383f32d60abb1451008f
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-03-15 17:48:03 -0500

    variable de entorno check

---
+++ parking-lot-frontend/config.js
@@ -0,0 +1,4 @@
+const config = {
+    baseURL: process.env.VUE_APP_BALANCER_URL
+};
+export default config;
--- parking-lot-frontend/src/main.js
+++ parking-lot-frontend/src/main.js
@@ -1,15 +1,15 @@
-
 window.__VUE_PROD_DEVTOOLS__ = false;
 window.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
 
-import axios from 'axios';
 import { createApp, ref } from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import config from '../config';
 
 const app = createApp(App);
 
-axios.defaults.baseURL = 'http://localhost:3000';
-console.log('Balanceador ',axios.defaults.baseURL)
+axios.defaults.baseURL = config.baseURL; 
+console.log('Balanceador ', axios.defaults.baseURL);
 
 app.config.globalProperties.$axios = axios;
 
@@ -24,4 +24,3 @@ app.mixin({
 });
 
 app.mount('#app');
-
